77f2023b8fec3bbf35fc92ff6a147ada5e3702be
Usama <usamamian7166@gmail.com>
2024-05-21 18:12:52 +0500

    After completing UI for Contact us page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,57 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 /*=============== SHOW CART ===============*/
-
+const cart = document.getElementById("cart");
+cartShop = document.getElementById("cart-shop");
+cartClose = document.getElementById("cart-close");
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
-
+if (cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add("show-cart");
+  });
+}
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
-
+if (cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove("show-cart");
+  });
+}
 /*=============== SHOW LOGIN ===============*/
-
+const login = document.getElementById("login");
+loginButton = document.getElementById("login-button");
+loginClose = document.getElementById("login-close");
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
-
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    login.classList.add("show-login");
+  });
+}
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
-
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    login.classList.remove("show-login");
+  });
+}
 /*=============== HOME SWIPER ===============*/
 
 var homeSwiper = new Swiper(".home_swiper", {
@@ -46,15 +76,41 @@ var newSwiper = new Swiper(".new-swiper", {
   slidesPerView: "auto",
   centeredSlides: true,
   loop: "true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
 });
 /*=============== SHOW SCROLL UP ===============*/
-
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 /*=============== LIGHT BOX ===============*/
 
 /*=============== QUESTIONS ACCORDION ===============*/
+const accordionItem = document.querySelectorAll(".question_item");
 
+accordionItem.forEach((item) => {
+  const accordionHeader = item.querySelector(".question_header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".questions_content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
 /*=============== STYLE SWITCHER ===============*/
