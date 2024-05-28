e4f70d99673056c6385277d8ecb18b399dd0f171
Usama <usamamian7166@gmail.com>
2024-05-28 13:11:19 +0500

    before adding the admin panel

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,3 +114,59 @@ const toggleItem = (item) => {
   }
 };
 /*=============== STYLE SWITCHER ===============*/
+
+/*=============== OTP Verification ===============*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const otpInputs = document.querySelectorAll(".otp_digit");
+
+  otpInputs.forEach((input, index) => {
+    input.addEventListener("input", (event) => {
+      const currentInput = event.target;
+      if (currentInput.value.length === 1 && index < otpInputs.length - 1) {
+        otpInputs[index + 1].focus();
+      }
+    });
+
+    input.addEventListener("keydown", (event) => {
+      const currentInput = event.target;
+      const key = event.key;
+      if (key === "Backspace" && currentInput.value.length === 0 && index > 0) {
+        otpInputs[index - 1].focus();
+      }
+    });
+  });
+});
+function sendOTP() {
+  // Simulate sending OTP to the email
+  // In a real application, this would be done server-side
+  let email = document.getElementById("email").value;
+  let otp_value = Math.floor(10000 + Math.random() * 90000);
+  let message_body = `Dear user,\n\nThank you for registering on fabX website. Your OTP for verification is: ${otp_value}\n\nPlease enter this OTP on the verification page to complete your registration.\n\nBest regards,\nThe fabX Team`;
+
+  Email.send({
+    SecureToken: "89624bd8-ba98-4283-9374-a8308b33be07",
+    To: email,
+    From: "musamarasheed7166@gmail.com",
+    Subject: "Email Verification",
+    Body: message_body,
+  }).then((message) => {
+    if (message === "OK") {
+      alert("OTP sent to Your email " + email);
+    }
+  });
+}
+// function generateOTP() {
+//   // Generate a random 5-digit OTP
+//   return Math.floor(10000 + Math.random() * 90000);
+// }
+// if (email) {
+//   const otp = generateOTP();
+//
+//   // Simulate sending email with OTP message
+//   console.log(message);
+//   document.getElementById("otpSentMessage").style.display = "block";
+//   document.querySelector(".verification_button").style.display = "block";
+// } else {
+//   alert("Please enter your email first.");
+// }
